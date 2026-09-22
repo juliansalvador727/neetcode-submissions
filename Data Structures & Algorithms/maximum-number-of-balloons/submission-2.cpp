6e60928d9ff0d4784f65508aa64501e7d6aa779c
julian <147367153+juliansalvador727@users.noreply.github.com>
2026-09-22 13:46:37 -0400

    Add: maximum-number-of-balloons - submission-2

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int b = 0; int a = 0; int l = 0; int o = 0; int n = 0;
+        for (int i = 0; i < text.length(); ++i) {
+            char c = text[i];
+            if (c == 'b') b++;
+            if (c == 'a') a++;
+            if (c == 'l') l++;
+            if (c == 'o') o++;
+            if (c == 'n') n++;
+        }
+        l /= 2;
+        o /= 2;
+        return min({b,a,l,o,n});
+    }
+};
